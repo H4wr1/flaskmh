d6ff431ae55e195f55460b22ca13fa2048ae2eb4
Mikolaj Hawryluk <hawrylukmikolaj@gmail.com>
2023-11-30 16:17:39 +0100

    trzeci

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, json
+from flask import Flask, json, request
 
 app = Flask(__name__)
 
+# Example memory storage
+studio_data = []
+
+# Load content of animations.json into studio_data
+try:
+    with open('animations.json', 'r') as file:
+        data = json.load(file)
+        studio_data = data.get("animations", [])
+except FileNotFoundError:
+    print("File not found. Using an empty list for studio_data.")
 
 @app.route("/")
 def hello():
@@ -61,3 +71,17 @@ def get_titles_by_keyword(keyword):
 
     except FileNotFoundError:
         return {"error": "File not found"}, 404
+
+@app.route('/studios/<int:id>', methods=['PUT'])
+def update_studio(id):
+    new_studio = request.args.get('studio')
+
+    # Check if the studio with the given ID exists
+    studio_to_update = next((studio for studio in studio_data if studio["ID"] == id), None)
+
+    if studio_to_update:
+        # Update the 'Studio' value
+        studio_to_update["Studio"] = new_studio
+        return {"message": f"Studio with ID {id} updated successfully", "data": studio_to_update}
+    else:
+        return {"error": f"Studio with ID {id} not found"}, 404
