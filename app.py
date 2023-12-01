821860da01b9c60167644cb6735726dce12b41f1
Mikolaj Hawryluk <hawrylukmikolaj@gmail.com>
2023-12-01 19:04:18 +0100

    ostatni

--- app.py
+++ app.py
@@ -2,57 +2,42 @@ from flask import Flask, json, request
 
 app = Flask(__name__)
 
-# Example memory storage
-studio_data = []
-
-# Load content of animations.json into studio_data
-try:
-    with open('animations.json', 'r') as file:
-        data = json.load(file)
-        studio_data = data.get("animations", [])
-except FileNotFoundError:
-    print("File not found. Using an empty list for studio_data.")
+with open('animations.json') as json_file:
+    data = json.load(json_file)
+    animations = data['animations']
 
 @app.route("/")
 def hello():
     return '<h1>Hello World!</h1>'
-
 
 @app.route('/titles', methods=['GET'])
 def get_original_titles():
     try:
         with open('animations.json', 'r') as file:
             result = []
-
-            data = json.load(file)
+            #data = json.load(file)
             for animation in data["animations"]:
                 result.append(animation['Original title'])
-            # original_titles = [item['Original title'] for item in data]
-
+ 
         return result
     except FileNotFoundError:
         return {"error": "File not found"}, 404
-
-
-
-
 
 @app.route('/ids/<int:animation_id>', methods=['GET'])
 def get_animation_by_id(animation_id):
         try:
             with open('animations.json', 'r') as file:
-                data = json.load(file)
+                #data = json.load(file)
                 animation = None
                 for ani in data['animations']:
                     if ani["ID"] == animation_id:
                         animation = ani
                         break
-
             if animation:
                 return animation
             else:
                 return {"error": f"Animacji z id {animation_id} nie ma"}, 404
-
+            
         except FileNotFoundError:
             return {"error": "File not found"}, 404
 
@@ -60,28 +45,26 @@ def get_animation_by_id(animation_id):
 def get_titles_by_keyword(keyword):
     try:
         with open('animations.json', 'r') as file:
-            data = json.load(file)
+            #data = json.load(file)
             result = []
 
             for ani in data["animations"]:
                 if keyword in ani["Keywords"]:
                     result.append(ani['Original title'])
-
             return result
 
     except FileNotFoundError:
         return {"error": "File not found"}, 404
 
-@app.route('/studios/<int:id>', methods=['PUT'])
-def update_studio(id):
+
+
+@app.route('/studios/<int:animation_id>', methods=['PUT'])
+def update_studio(animation_id):
     new_studio = request.args.get('studio')
+    animation = next((a for a in animations if a['ID'] == animation_id), None)
 
-    # Check if the studio with the given ID exists
-    studio_to_update = next((studio for studio in studio_data if studio["ID"] == id), None)
-
-    if studio_to_update:
-        # Update the 'Studio' value
-        studio_to_update["Studio"] = new_studio
-        return {"message": f"Studio with ID {id} updated successfully", "data": studio_to_update}
+    if animation:
+        animation['Studio'] = new_studio
+        return {'message': f'Zmienione studio dla ID: {animation_id}','updated_data': animation}, 200
     else:
-        return {"error": f"Studio with ID {id} not found"}, 404
+        return {'error': f'Nie znaleziono ID: {animation_id}'}, 404
